9fd4e1e62bc7f214804d25388ca0dbbf8af7c2c1
kta.carrasco <kta.carrasco@gmail.com>
2020-08-13 16:55:27 -0400

    entrega hasta las 17:00 hrs con recyclerview arreglado y toast

--- app/src/main/java/com/example/myappciclobike/MainActivity.kt
+++ app/src/main/java/com/example/myappciclobike/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.myappciclobike
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import android.widget.Toast.*
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ciromine.ciclobike.Ciclovia
 import kotlinx.android.synthetic.main.cyclo_item_list.*
+import android.widget.Toast.makeText as toastMakeText
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var layoutManager: RecyclerView.LayoutManager
     private lateinit var nDataset: MutableList<Ciclovia>
 
+    @SuppressLint("ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,18 +32,22 @@ class MainActivity : AppCompatActivity() {
       //  Log.d("LOG",SetupCiclovias().init1().toString())
 
         val filtrarlascondes : Button= findViewById(R.id.buttonLascondes)
+
+
         val sinfiltro : Button=findViewById(R.id.buttonallcomunas)
        layoutManager = LinearLayoutManager(this)
         nDataset  = SetupCiclovias().init()as MutableList<Ciclovia>
         adapter = CycloAdapter(nDataset)
-        recyclerView = findViewById(R.id.textViewCyclo)
+       recyclerView = findViewById(R.id.recyclo)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = layoutManager
 
     filtrarlascondes.setOnClickListener(View.OnClickListener {
-        when (nDataset) {
+        Toast.makeText(this,"SOLO LAS CONDES", LENGTH_LONG).show()
+      //  when (nDataset) {
+
         //val filtrarlascondes = String nDataset.filter { comuna ->comuna.equals("las condes") }
-    }
+    //}
 
     })
 
